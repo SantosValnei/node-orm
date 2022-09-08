08aef33571c46d001541cfa31d40e8202318bd8a
Valnei Santos <valnei.silva@unidompedro.com>
2022-09-08 18:29:38 -0300

    adicionando rota pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,15 +1,14 @@
 const database = require('../models');
 
 class PessoaController {
-    static async pegaTodasPessoas (req, res) {
+    static async pegaTodasAsPessoas (req, res) {
         try {
             const todasAsPessoas = await database.Pessoas.findAll();
             return res.status(200).json(todasAsPessoas);
         } catch (error) {
             return res.status(404).json(error.message)
-        }
-        
+        }                                                           
     }
 }
 
-module.exports = PessoaController;
+module.exports = PessoaController                                      
